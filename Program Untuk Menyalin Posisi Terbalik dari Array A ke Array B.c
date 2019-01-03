5b72c99d107e4c119b279cd7bf4acf37096e90b9
roni46 <39713502+roni46@users.noreply.github.com>
2019-01-03 18:23:51 +0700

    Add files via upload

---
+++ Program Untuk Menyalin Posisi Terbalik dari Array A ke Array B.c
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void)
+{
+	int c[5]={5,10,6,0,4};
+	int d[5];
+	int i=0, j=0;
+    printf("Nilai array yang akan ditampilkan\n");
+    for(i=4; i>=0;i--)
+{
+	d[i]=c[j];
+	printf("%d",c[j]);
+	j++;
+}
+printf("posisi terbalik dari array A ke array B sebagai Berikut\n");
+for(i=0;i<5;i++)
+{
+	printf("%d",d[i]);
+}
+printf("\n");
+system("pause");
+return 0;
+}
